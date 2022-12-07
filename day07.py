59b95781ea14353ba770cae8af41917bfe8b7954
Rebekah 'hellmak <79146603+hellmak@users.noreply.github.com>
2022-12-07 23:18:06 +0100

    Commiting

---
+++ day07.py
@@ -0,0 +1,62 @@
+#work in progress
+
+input_file = """$ cd /
+$ ls
+268495 jgfbgjdb
+dir ltcqgnc
+272455 pct.bbd
+200036 phthcq
+174378 qld
+dir rbmstsf
+130541 trhbvp.fmm
+dir twjcmp
+$ cd ltcqgnc
+$ ls
+227634 brjmpbfg.hjh
+dir cmdzcq
+dir dnbf
+203609 fpj
+dir frt
+288222 lcr.nlr
+dir ngm
+dir ngsrlzc
+5927 phrnnw.dzj
+dir shjv
+dir wsvfbb
+$ cd cmdzcq
+$ ls
+37316 cfvhc.qsw
+41839 mgwlr
+dir pfmbt
+281659 tlpqzz
+dir vmd
+189667 zdvj.sbl
+$ cd pfmbt
+$ ls
+120151 bhzsnw.gft
+45035 cfvhc.zpp"""
+
+input_list  = input_file.split("\n")
+
+### psudo:
+def first_function(x):
+    for item in x:
+    item
+    first_char = item[0]
+
+    if      first_char == "$":
+        if      first_char == "c"
+            if      item == "cd .."
+                change_dir_up()
+            else:
+                change_dir_to(item[3:])
+        elif    item == "ls"
+
+        
+
+    elif    item == "ls":
+    elif    first_char == is_num[first_char]:
+    else:
+        exit("error:001")
+
+print("pt:" + str(first_function(input_list)))
